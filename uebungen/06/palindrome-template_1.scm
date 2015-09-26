d6ecb8f9ba0f73fd7583a6f654960983bde6336a
gitfvb <fvb@ritterstation.de>
2015-09-26 03:06:56 +0200

    removed bin files and restructured the folders

--- uebungen/06/palindrome-template_1.scm
+++
@@ -1,9 +0,0 @@
-;; Contract: palindrome: (listof x) -> (listof x)
-;; Purpose:  to build a list which transforms the given
-;;           list into a palindrome list..
-;; Examples 1.) (palindrome (list 'a 'b 'c)) 
-;;                 should produce: (list 'a 'b 'c 'b 'a)
-;;          2.) (palindrome (list 1 2 3 4))
-;;                 should produce: (list 1 2 3 4 3 2 1)
-(define (palindrome alox)
-   (...))
--- uebungen/11/TemperatureClient_0.java
+++
@@ -1,9 +0,0 @@
-/**
- * This interface must be implemented by all clients interested in receiving
- * temperature data 
- *
- * @author Daniel Schreiber
- */
-public interface TemperatureClient {
-  void newTemperature(long timestamp, String location, double temperature);
-}
